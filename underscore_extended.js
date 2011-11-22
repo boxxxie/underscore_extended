1a6a2eea69d4643e685b2307c1064ccacb6d84c2
Paul <somethingitalian@gmail.com>
2011-11-21 19:35:41 -0500

    added unEscape function and removeKeys (similar to selectKeys) cleaned up the selectKeys function to make it easier to read

--- underscore_extended.js
+++ underscore_extended.js
@@ -1,13 +1,10 @@
 _.mixin({
-	    //Retrieve the keys and values of an object's properties.
-	    //{key: val, key1: val1...} -> [[key,val],[key1,val1]...]
+	    // Retrieve the keys and values of an object's properties.
 	    kv:function (obj) {
-		if(typeof obj !== 'object'){return null;}
 		return _.zip(_.keys(obj),_.values(obj));}});
 
 _.mixin({
 	    //converts an array of pairs into an objcet
-	    //[[key,val],[key1,val1]...] -> {key: val, key1: val1...}
 	    toObject:function(pairs){
 		return pairs.reduce(function(total,cur){
 					var key = _.first(cur),
@@ -19,6 +16,43 @@ _.mixin({
 _.mixin({
 	    //create an object with only the keys in the selected keys array arg
 	    selectKeys:function (obj,keys){
-		return _.toObject(_.kv(obj)
-		    .filter(function(kv){return _.contains(keys,_.first(kv));}));
+		return  _(obj).chain()
+		    .kv()
+		    .filter(function(kv){return _.contains(keys,_.first(kv));})
+		    .toObject()
+		    .value();
+	    }});
+
+_.mixin({
+	    //create an object without the keys in the selected keys array arg
+	    removeKeys:function (obj,keys){
+		return _(obj).chain()
+		    .kv()
+		    .reject(function(kv){return _.contains(keys,_.first(kv));})
+		    .toObject()
+		    .value();
+	    }});
+
+
+// unEscape a string for HTML interpolation.
+/*
+ *   _.escape = function(string) {
+ return (''+string)
+ .replace(/&(?!\w+;|#\d+;|#x[\da-f]+;)/gi, '&amp;')
+ .replace(/</g, '&lt;')
+ .replace(/>/g, '&gt;')
+ .replace(/"/g, '&quot;')
+ .replace(/'/g, '&#x27;')
+ .replace(/\//g,'&#x2F;');
+ };
+ * */
+_.mixin({
+	    unEscape : function(string) {
+		return (''+string)
+		    .replace(/&lt;/g,    '<')
+		    .replace(/&gt;/g,    '>')
+		    .replace(/&quot;/g,  '"')
+		    .replace(/&#x27;/g,  "'")
+		    .replace(/&#x2F;/g,  '\/')
+		    .replace(/&amp;/g,   '&');
 	    }});
